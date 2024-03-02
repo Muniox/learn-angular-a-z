a52d3da2ade7a8e07cd3e3c9b475ddb445ddfa60
Muniox <muniox20@gmail.com>
2024-03-02 21:51:23 +0100

    Binding events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'learn-angular-a-z';
+  items: WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Find grass that cuts itself'),
+  ];
+  title = 'wishlist';
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+  }
+
+  addNewWish(text: string) {
+    const newWish = new WishItem(text);
+    this.items.push(newWish);
+  }
 }
