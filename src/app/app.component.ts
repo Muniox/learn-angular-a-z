e97d156dc8affa5f39a0c23b45da0cdde2a86841
Muniox <muniox20@gmail.com>
2024-03-03 12:55:16 +0100

    filter wishes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,29 @@ export class AppComponent {
     new WishItem('Get coffee', true),
     new WishItem('Find grass that cuts itself'),
   ];
-  title = 'wishlist';
+  title: string = 'wishlist';
+  newWishText: string = '';
+  listFilter: string = '0';
+  get visibleItems(): WishItem[] {
+    const value = this.listFilter;
+    switch (value) {
+      case '0':
+        return this.items;
+      case '1':
+        return this.items.filter((item) => !item.isComplete);
+      case '2':
+        return this.items.filter((item) => item.isComplete);
+      default:
+        return this.items;
+    }
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
   }
 
-  addNewWish(text: string) {
-    const newWish = new WishItem(text);
-    this.items.push(newWish);
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
   }
 }
