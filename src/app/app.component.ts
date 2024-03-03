1583cc742a6f05826af4d63faf7f60233b98d5d5
Muniox <muniox20@gmail.com>
2024-03-03 14:45:54 +0100

    Enovato's way of writing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,27 +12,5 @@ export class AppComponent {
     new WishItem('Get coffee', true),
     new WishItem('Find grass that cuts itself'),
   ];
-  title: string = 'wishlist';
-  listFilter: string = '0';
-  get visibleItems(): WishItem[] {
-    const value = this.listFilter;
-    switch (value) {
-      case '0':
-        return this.items;
-      case '1':
-        return this.items.filter((item) => !item.isComplete);
-      case '2':
-        return this.items.filter((item) => item.isComplete);
-      default:
-        return this.items;
-    }
-  }
-
-  setListFilter(value: string) {
-    this.listFilter = value;
-  }
-
-  addWishToItems(item: WishItem) {
-    this.items.push(item);
-  }
+  filter = () => {};
 }
--- src/app/wish-filter/wish-filter.component.ts
+++ src/app/wish-filter/wish-filter.component.ts
@@ -1,11 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { WishItem } from '../../shared/models/wishItem';
+
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete,
+];
 
 @Component({
   selector: 'app-wish-filter',
   templateUrl: './wish-filter.component.html',
   styleUrl: './wish-filter.component.css',
 })
-export class WishFilterComponent {
-  @Output() filter = new EventEmitter<string>();
-  listFilter = '0';
+export class WishFilterComponent implements OnInit {
+  @Output() filter = new EventEmitter();
+  listFilter: string = '0';
+
+  changeFilter(value: string) {
+    return this.filter.emit(filters[Number(value)]);
+  }
+
+  ngOnInit(): void {
+    this.changeFilter('0');
+  }
 }
