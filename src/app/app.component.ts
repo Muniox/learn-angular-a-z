fbb6ca3debfbaa71473c4bf29652526f16e095b4
Muniox <muniox20@gmail.com>
2024-03-05 20:44:07 +0100

    Exporting single component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { WishItem } from '../shared/models/wishItem';
-import { EventService } from './../shared/services/eventService';
-import { WishService } from './wish/wish.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  items: WishItem[] = [];
-  filter: any;
-
-  constructor(
-    private events: EventService,
-    private wishService: WishService,
-  ) {
-    this.events.listen('removeWish', (wish: any) => {
-      const index = this.items.indexOf(wish);
-      this.items.splice(index, 1);
-    });
-  }
-
-  ngOnInit(): void {
-    this.wishService.getWishes().subscribe({
-      next: (data: any) => {
-        this.items = data;
-      },
-      error: (error: Error) => {
-        alert(error.message);
-      },
-      // complete: () => {
-      //   alert('data loaded');
-      // },
-    });
-  }
-}
+export class AppComponent {}
---
+++ src/app/wish/wish.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { WishItem } from '../../shared/models/wishItem';
+import { EventService } from '../../shared/services/eventService';
+import { WishService } from './wish.service';
+
+@Component({
+  selector: 'app-wish',
+  templateUrl: './wish.component.html',
+  styleUrl: './wish.component.css',
+})
+export class WishComponent implements OnInit {
+  items: WishItem[] = [];
+  filter: any;
+
+  constructor(
+    private events: EventService,
+    private wishService: WishService,
+  ) {
+    this.events.listen('removeWish', (wish: any) => {
+      const index = this.items.indexOf(wish);
+      this.items.splice(index, 1);
+    });
+  }
+
+  ngOnInit(): void {
+    this.wishService.getWishes().subscribe({
+      next: (data: any) => {
+        this.items = data;
+      },
+      error: (error: Error) => {
+        alert(error.message);
+      },
+      // complete: () => {
+      //   alert('data loaded');
+      // },
+    });
+  }
+}
