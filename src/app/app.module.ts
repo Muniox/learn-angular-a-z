28672b9b66adc6e7970c37a509742eff90fff4ce
Muniox <muniox20@gmail.com>
2024-03-05 21:58:19 +0100

    Introducing Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WishModule } from './wish/wish.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, WishModule],
+  imports: [BrowserModule, AppRoutingModule, WishModule, ContactModule],
   providers: [],
   bootstrap: [AppComponent],
 })
