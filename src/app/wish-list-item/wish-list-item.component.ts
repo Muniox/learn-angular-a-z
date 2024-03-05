9b29e0638c751469b60c8518f01a94f222fc52a1
Muniox <muniox20@gmail.com>
2024-03-05 10:56:33 +0100

    Event Bus in Angular

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import events from './../../shared/services/eventService';
 
 @Component({
   selector: 'app-wish-list-item',
@@ -9,6 +10,18 @@ export class WishListItemComponent {
   @Input() wishText!: string; //wykrzyknik oznacza, że ta wartość nie jest nullem
   @Input() fullfilled!: boolean;
   @Output() fullfilledChange = new EventEmitter<boolean>();
+
+  constructor() {
+    events.listen('removeWish', (wish: any) => {
+      // todo remove wish from items
+
+      console.log(wish);
+    });
+  }
+
+  removeWish() {
+    events.emit('removeWish', this.wishText);
+  }
 
   toggleFullfilled() {
     this.fullfilled = !this.fullfilled;
