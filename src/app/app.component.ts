8483eef16edb493a739eb62c174a18f2984b6fba
Muniox <muniox20@gmail.com>
2024-03-05 12:14:56 +0100

    Basic http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WishItem } from '../shared/models/wishItem';
 import { EventService } from './../shared/services/eventService';
+import { WishService } from './wish.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  items: WishItem[] = [
-    new WishItem('To Learn Angular'),
-    new WishItem('Get coffee', true),
-    new WishItem('Find grass that cuts itself'),
-  ];
+export class AppComponent implements OnInit {
+  items!: WishItem[];
   filter: any;
 
-  constructor(private events: EventService) {
+  constructor(
+    private events: EventService,
+    private wishService: WishService,
+  ) {
     this.events.listen('removeWish', (wish: any) => {
       const index = this.items.indexOf(wish);
       this.items.splice(index, 1);
+    });
+  }
+
+  ngOnInit(): void {
+    this.wishService.getWishes().subscribe((data: any) => {
+      this.items = data;
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
@@ -17,7 +18,7 @@ import { WishListItemComponent } from './wish-list-item/wish-list-item.component
     WishFilterComponent,
     WishListItemComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
