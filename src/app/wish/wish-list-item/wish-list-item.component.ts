8feb6d14e03f0a3e4a30a11a89dedd606b7bdd34
Muniox <muniox20@gmail.com>
2024-03-06 14:51:51 +0100

    cleared

--- src/app/wish/wish-list-item/wish-list-item.component.ts
+++ src/app/wish/wish-list-item/wish-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { EventService } from '../../../shared/services/EventService';
 import { WishItem } from '../../../shared/models/wishItem';
 
