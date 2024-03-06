4110c30321e4d4906a4550f2e5da216704e14e2e
Muniox <muniox20@gmail.com>
2024-03-06 13:28:18 +0100

    Handling not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WishComponent } from './wish/wish.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WishComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent }, //kolejność ma znaczenie
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WishModule } from './wish/wish.module';
 import { ContactModule } from './contact/contact.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, WishModule, ContactModule],
   providers: [],
   bootstrap: [AppComponent],
